8457b6ab9b3dc46b7c1a1e526da477ff4bd57f30
xxxJotaxxx <joelruizlopez6@gmail.com>
2021-11-06 16:02:06 +0100

    format code

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,23 @@ void setup()
 void loop()
 {
     VALOR = digitalRead(SENSOR); // lee valor de sensor y asigna a variable VALOR
+
+    // si el valor es distinto del ultimo
     if (VALOR != ANTERIOR)
-    {                                // si el valor es distinto del ultimo
-        if (VALOR == HIGH)           // si VALOR tiene un nivel alto es linea negra
+    {
+        // si VALOR tiene un nivel alto es linea negra
+        if (VALOR == HIGH)
+        {
             Serial.println("Linea"); // imprime en monitor serial la palabra Linea
-        else                         // si VALOR tiene un nivel bajo es fuera de linea
+        }
+
+        // si VALOR tiene un nivel bajo no es linea negra
+        else
+        {
             Serial.println("Fuera"); // imprime en monitor serial la palabra Fuera
-        ANTERIOR = VALOR;            // actualiza variable ANTERIOR con el actual de VALOR
+            ANTERIOR = VALOR;        // actualiza variable ANTERIOR con el actual de VALOR
+        }
     }
+
     delay(50); // espera entre lectura y lectura
 }
